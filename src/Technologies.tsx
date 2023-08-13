2aac3e09800fcdd9d2616f906cf95ba8d776ba91
MrUnderSan <MrUnderSan@gmail.com>
2023-08-13 09:10:21 +0300

    completed 10 lessons

---
+++ src/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
