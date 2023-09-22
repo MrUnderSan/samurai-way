141e6ca626eefc047a64bc4e8a79f63cfc0df3a9
MrUnderSan <MrUnderSan@gmail.com>
2023-09-22 11:18:50 +0300

    refactor: moved BrowserRouter from App to Index

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Dialogs} from './components/Dialogs/Dialogs';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import {Settings} from './components/Settings/Settings';
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
@@ -53,7 +53,7 @@ const App: React.FC<PropsType> = ({state, addPost}) => {
     const {profilePage, messagesPage, sidebar} = state
 
     return (
-        <BrowserRouter>
+
             <div className="app-wrapper">
 
                 <Header/>
@@ -68,7 +68,7 @@ const App: React.FC<PropsType> = ({state, addPost}) => {
                 </div>
 
             </div>
-        </BrowserRouter>
+
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {addPost, state} from './redux/state';
+import {BrowserRouter} from 'react-router-dom';
 
 
 ReactDOM.render(
-    <App state={state} addPost={addPost}/>,
+    <BrowserRouter>
+        <App state={state} addPost={addPost}/>
+    </BrowserRouter>,
     document.getElementById('root')
 );
