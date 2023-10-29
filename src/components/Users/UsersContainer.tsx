a247080473c636c686fa6902412c301bfc7981c9
MrUnderSan <MrUnderSan@gmail.com>
2023-10-29 20:33:19 +0300

    completed lesson 53 (extends React.Component)

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,8 +1,8 @@
 import {connect} from 'react-redux';
 import {AppStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
-import {Users} from './Users';
 import {followAC, setUsersAC, unfollowAC, UserType} from '../../redux/users-reducer';
+import Users from './Users';
 
 type MapStateToPropsType = {
     users: UserType[]
